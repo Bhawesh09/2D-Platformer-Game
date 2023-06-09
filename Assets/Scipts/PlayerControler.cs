eccf62ec6d320d6dc0a9fa8f84e43a90124d13a5
Bhawesh09 <bhaweshagarwal09@gmail.com>
2023-06-09 16:09:46 +0530

    Player Animation based on input

--- Assets/Scipts/PlayerControler.cs
+++ Assets/Scipts/PlayerControler.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private Animator playerAnimator;
+
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerAnimator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        speed = Input.GetAxisRaw("Horizontal");
+        if (speed < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (speed > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        playerAnimator.SetFloat("Speed", Mathf.Abs(speed));
     }
 }
